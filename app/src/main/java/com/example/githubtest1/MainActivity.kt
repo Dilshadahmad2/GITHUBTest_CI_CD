e2159e34f5ea236dfe9e7cda6958d7b8456bb436
Dilshad <dilshad.work@gmail.com>
2022-03-13 23:59:47 +0530

    Initial commit

---
+++ app/src/main/java/com/example/githubtest1/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.githubtest1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun addFirstFunction(){
+        Log.i("","")
+    }
+    fun addSecondFunction(){
+        var s =""
+    }
+}
