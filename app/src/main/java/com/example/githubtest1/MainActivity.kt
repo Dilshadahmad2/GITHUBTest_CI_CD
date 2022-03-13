fbc464ecf39f0daf3e9cb6b606677582205ba99f
Dilshad <dilshad.work@gmail.com>
2022-03-14 00:54:08 +0530

    k3

--- app/src/main/java/com/example/githubtest1/MainActivity.kt
+++ app/src/main/java/com/example/githubtest1/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     fun addFirstFunction(){
         Log.i("","")
         Log.i("","")
+        Log.i("","")
     }
     fun addSecondFunction(){
         var s =""
